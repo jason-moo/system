1d9b03990304ec7ac1e54bc645c88bb49f972e8d
jason_moo <504196131@qq.com>
2016-11-26 12:22:32 +0800

    修改pom

--- system-client/src/main/java/me/gacl/service/SystemTempleService.java
+++ system-client/src/main/java/me/gacl/service/SystemTempleService.java
@@ -5,13 +5,13 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 public interface SystemTempleService {
 	
-	public Object set(Object key,Object value);
-	
-	public Object set(Object key,Object value,long time);
-	
-	public Object get(Object key);
-	
-	public boolean delete(Object key);
-	
-	public RedisConnectionFactory getFactory();
+//	public Object set(Object key,Object value);
+//
+//	public Object set(Object key,Object value,long time);
+//
+//	public Object get(Object key);
+//
+//	public boolean delete(Object key);
+//
+//	public RedisConnectionFactory getFactory();
 }
